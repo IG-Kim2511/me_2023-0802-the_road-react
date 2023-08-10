a2460d0a94ba5c4c5ed9c81f7397d3164863d4b8
IG <iikim2511@gmail.com>
2023-08-10 17:41:31 -0400

    populartor-ing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Header from './components/Header'
+import PopularTours from './components/PopularTours'
 
 
 
@@ -13,6 +14,7 @@ const App = () => {
     
         <Navbar/>
         <Header/>
+        <PopularTours/>
     
     </div>
   )
--- src/components/PopularTours.js
+++ src/components/PopularTours.js
@@ -1,71 +1,72 @@
 import React from 'react'
+import './PopularTours.css'
 
 const PopularTours = () => {
   return (
     <div>
-        {/* 
-        <section class="popular-tours">
-        <h1 class="popular-tours-heading">The Most Popular Tours</h1>
-        <div class="cards-wrapper">
-            <div class="card">
-            <div class="front-side">
-                <img src="images/forest.jpg" alt="Forest" class="card-image" />
-                <h1 class="tour-name">The wild forest</h1>
-                <ul class="card-list">
-                <li class="card-list-item">7 days tour</li>
-                <li class="card-list-item">Up to 20 people</li>
-                <li class="card-list-item">4 tour guides</li>
-                <li class="card-list-item">Sleep in private tents</li>
-                <li class="card-list-item">Difficulty: medium</li>
+     
+        <section className="popular-tours">
+        <h1 className="popular-tours-heading">The Most Popular Tours</h1>
+        <div className="cards-wrapper">
+            <div className="card">
+            <div className="front-side">
+                <img src="images/forest.jpg" alt="Forest" className="card-image" />
+                <h1 className="tour-name">The wild forest</h1>
+                <ul className="card-list">
+                <li className="card-list-item">7 days tour</li>
+                <li className="card-list-item">Up to 20 people</li>
+                <li className="card-list-item">4 tour guides</li>
+                <li className="card-list-item">Sleep in private tents</li>
+                <li className="card-list-item">Difficulty: medium</li>
                 </ul>
-                <button class="navigation-button">price &gt;&gt;</button>
+                <button className="navigation-button">price &gt;&gt;</button>
             </div>
-            <div class="back-side center">
-                <button class="navigation-button">&lt;&lt; back</button>
-                <h3 class="tour-price">$399</h3>
-                <button class="card-button">Booking</button>
+            <div className="back-side center">
+                <button className="navigation-button">&lt;&lt; back</button>
+                <h3 className="tour-price">$399</h3>
+                <button className="card-button">Booking</button>
             </div>
             </div>
-            <div class="card">
-            <div class="front-side">
-                <img src="images/river.jpg" alt="River" class="card-image" />
-                <h1 class="tour-name">Along the river</h1>
-                <ul class="card-list">
-                <li class="card-list-item">9 days tour</li>
-                <li class="card-list-item">Up to 30 people</li>
-                <li class="card-list-item">7 tour guides</li>
-                <li class="card-list-item">Sleep in private tents</li>
-                <li class="card-list-item">Difficulty: hard</li>
+            <div className="card">
+            <div className="front-side">
+                <img src="images/river.jpg" alt="River" className="card-image" />
+                <h1 className="tour-name">Along the river</h1>
+                <ul className="card-list">
+                <li className="card-list-item">9 days tour</li>
+                <li className="card-list-item">Up to 30 people</li>
+                <li className="card-list-item">7 tour guides</li>
+                <li className="card-list-item">Sleep in private tents</li>
+                <li className="card-list-item">Difficulty: hard</li>
                 </ul>
-                <button class="navigation-button">price &gt;&gt;</button>
+                <button className="navigation-button">price &gt;&gt;</button>
             </div>
-            <div class="back-side center">
-                <button class="navigation-button">&lt;&lt; back</button>
-                <h3 class="tour-price">$499</h3>
-                <button class="card-button">Booking</button>
+            <div className="back-side center">
+                <button className="navigation-button">&lt;&lt; back</button>
+                <h3 className="tour-price">$499</h3>
+                <button className="card-button">Booking</button>
             </div>
             </div>
-            <div class="card">
-            <div class="front-side">
-                <img src="images/sea.jpg" alt="Sea" class="card-image" />
-                <h1 class="tour-name">The island beach</h1>
-                <ul class="card-list">
-                <li class="card-list-item">5 days tour</li>
-                <li class="card-list-item">Up to 40 people</li>
-                <li class="card-list-item">8 tour guides</li>
-                <li class="card-list-item">Sleep in hotel</li>
-                <li class="card-list-item">Difficulty: easy</li>
+            <div className="card">
+            <div className="front-side">
+                <img src="images/sea.jpg" alt="Sea" className="card-image" />
+                <h1 className="tour-name">The island beach</h1>
+                <ul className="card-list">
+                <li className="card-list-item">5 days tour</li>
+                <li className="card-list-item">Up to 40 people</li>
+                <li className="card-list-item">8 tour guides</li>
+                <li className="card-list-item">Sleep in hotel</li>
+                <li className="card-list-item">Difficulty: easy</li>
                 </ul>
-                <button class="navigation-button">price &gt;&gt;</button>
+                <button className="navigation-button">price &gt;&gt;</button>
             </div>
-            <div class="back-side center">
-                <button class="navigation-button">&lt;&lt; back</button>
-                <h3 class="tour-price">$599</h3>
-                <button class="card-button">Booking</button>
+            <div className="back-side center">
+                <button className="navigation-button">&lt;&lt; back</button>
+                <h3 className="tour-price">$599</h3>
+                <button className="card-button">Booking</button>
             </div>
             </div>
         </div>
-        </section> */}
+        </section> 
     </div>
   )
 }
