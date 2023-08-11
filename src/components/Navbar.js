bc88245cb1653667b73e11241fae2cd827bc790d
IG <iikim2511@gmail.com>
2023-08-11 16:10:00 -0400

    navbar-ing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "./Navbar.css"
+
+import navbarBg from '../assets/navbar-bg.jpg'
 
 const Navbar = () => {
+
+
+  const styleNavbar ={
+    background: `url(${navbarBg})`
+  }
+  
+
+  const [igloo, setIgloo] = useState(false)
+  console.log(igloo)
+
   return (
     <div>
   {/*      [navbar] 
        CSS 26 : center 
        js 06  */}
-      <div class="open-navbar-icon navbar-icon center">
+      <div class="open-navbar-icon navbar-icon center" onClick={()=>{setIgloo(!igloo)}}>
         <div class="line" ></div>
         <div class="line" ></div>
         <div class="line" ></div>
       </div>
+
       {/*  [navbar wrap] 클릭해서 x모양으로 바뀔 때 */}
       <div class="navbar-wrapper">
-        <nav class="navbar">
+        <nav class="navbar" style={styleNavbar}>
       {/*      CSS 26 : center  */}
           <div class="close-navbar-icon navbar-icon center">
             <div class="line line-1"></div>
